8ae54f948e4a010f1a08d3a24cc761302033a7b9
Mattia Bolognini <mat.bolo10@gmail.com>
2026-04-23 10:11:30 +0200

    Ignore secrets' header

--- Greenhouse/src/Secrets.h
+++ Greenhouse/src/Secrets.h
@@ -2,13 +2,13 @@
 #define SECRETS_H
 
 // WiFi
-#define SECRET_SSID "MyWifiSSID"
-#define SECRET_PASS "MyWifiPassword"
+#define SECRET_SSID "IoTLabThingsU14"
+#define SECRET_PASS "L@b%I0T*Ui4!P@sS**0%Lessons!"
 
 // InfluxDB
-#define INFLUXDB_URL "http://MyIP:MyPort"
-#define INFLUXDB_TOKEN "MyToken"
-#define INFLUXDB_ORG "MyOrg"
-#define INFLUXDB_BUCKET "MyBucket"
+#define INFLUXDB_URL "http://149.132.176.75:8086"
+#define INFLUXDB_TOKEN "FlcfbYuuR_hoX26ghlqvtD0wDJ4p-9yaySFndn2u2hi591m42ZRm_BZSkGqf575RUSKzoHAWMZ87ry5uk7mi9Q=="
+#define INFLUXDB_ORG "labiot-org"
+#define INFLUXDB_BUCKET "GiacomoMaggioni-bucket"
 
 #endif
