40375ff4700693ad35cb6733208c9e4882d97b68
Mattia Bolognini <mat.bolo10@gmail.com>
2026-04-23 10:09:15 +0200

    Move secrets to a dedicated untracked header, and give a template secrets' header

--- Greenhouse/src/Params.h
+++ Greenhouse/src/Params.h
@@ -2,6 +2,8 @@
 #ifndef PARAMS_H
 #define PARAMS_H
 
+#include "Secrets.h"
+
 /*-------------------------------
 ------SENSORS AND ACTUATORS------
 -------------------------------*/
@@ -40,18 +42,9 @@
 #define DISPLAY_UPDATE_INTERVAL 2000
 
 /*-------------------------------
------------WIFI e DB-------------
+------------------DB-------------
 -------------------------------*/
 
-// WiFi
-#define SECRET_SSID "IoTLabThingsU14"
-#define SECRET_PASS "L@b%I0T*Ui4!P@sS**0%Lessons!"
-
-// InfluxDB
-#define INFLUXDB_URL "http://149.132.176.75:8086"
-#define INFLUXDB_TOKEN "FlcfbYuuR_hoX26ghlqvtD0wDJ4p-9yaySFndn2u2hi591m42ZRm_BZSkGqf575RUSKzoHAWMZ87ry5uk7mi9Q=="
-#define INFLUXDB_ORG "labiot-org"
-#define INFLUXDB_BUCKET "GiacomoMaggioni-bucket"
 #define INFLUXDB_UPDATE_INTERVAL 15000
 #define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
 
---
+++ Greenhouse/src/Secrets.h
@@ -0,0 +1,14 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi
+#define SECRET_SSID "MyWifiSSID"
+#define SECRET_PASS "MyWifiPassword"
+
+// InfluxDB
+#define INFLUXDB_URL "http://MyIP:MyPort"
+#define INFLUXDB_TOKEN "MyToken"
+#define INFLUXDB_ORG "MyOrg"
+#define INFLUXDB_BUCKET "MyBucket"
+
+#endif
